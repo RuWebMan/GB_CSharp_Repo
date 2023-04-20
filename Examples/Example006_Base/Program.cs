5bf5dad4c00e161a694b5e9ac60b8dc507b016d3
VicS <ruwebman@gmail.com>
2023-04-20 14:43:04 +0300

    Add Example 006 Base

---
+++ Examples/Example006_Base/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 1;
+int b = 2;
+int c = 19;
+int d = 7;
+int e = 11;
+int max = a;
+
+if (max < a ) max = a;
+if (max < b ) max = b;
+if (max < c ) max = c;
+if (max < d ) max = d;
+if (max < e ) max = e;
+
+Console.Write("Max = ");
+Console.WriteLine(max);
+
+
