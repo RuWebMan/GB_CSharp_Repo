9c329262ff0b8c7fe7a7e66b76ae631c16fb2d66
VicS <ruwebman@gmail.com>
2023-04-21 14:51:41 +0300

    Add task №4

--- Seminars/HW_Lesson№1/Program.cs
+++ Seminars/HW_Lesson№1/Program.cs
@@ -3,17 +3,42 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+// Console.Write("Введите число №1: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число №2: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+// {
+//     Console.WriteLine("Число №1 больше числа №2");
+// }
+// else
+// {
+//     Console.WriteLine("Число №2 больше числа №1");
+// }
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
 Console.Write("Введите число №1: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число №2: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число №3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 > num2)
+if (num1 > num2 && num1 > num3 )
 {
-    Console.WriteLine("Число №1 больше числа №2");
+    Console.WriteLine("Число №1 - максимальное число");
+}
+else if (num2 > num1 && num2 > num3 )
+{
+    Console.WriteLine("Число №2 - максимальное число");
 }
 else
 {
-    Console.WriteLine("Число №2 больше числа №1");
+    Console.WriteLine("Число №3 - максимальное число");
 }
-
