d19f07a91f69a305bf3d522abd7f5d3b8d6055ed
VicS <ruwebman@gmail.com>
2023-04-21 23:11:01 +0300

    Add task №8

--- Seminars/HW_Lesson№1/Program.cs
+++ Seminars/HW_Lesson№1/Program.cs
@@ -43,19 +43,38 @@
 //     Console.WriteLine("Число №3 - максимальное число");
 // }
 
+
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
 
-Console.Write("Введите число : ");
-int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число : ");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-if (num % 2 == 0)
-{
-    Console.WriteLine("Введенное число - четное");
-}
-else
-{
-    Console.WriteLine("Введенное число - нечетное");
-}
+// if (num % 2 == 0)
+// {
+//     Console.WriteLine("Введенное число - четное");
+// }
+// else
+// {
+//     Console.WriteLine("Введенное число - нечетное");
+// }
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.Write("Введите число N:");
+// int num = Convert.ToInt32(Console.ReadLine()); //int.Parse(Console.ReadLine());
+// int count = 1;
+
+// while (count <= num)
+// {
+//     if (count % 2 == 0)
+//        {
+//          Console.WriteLine(count);
+//        }
+//         count++;
+//  }
