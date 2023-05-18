96502ec6ab6f3b54c9a729c42381c1e6d24ef935
VicS <ruwebman@gmail.com>
2023-05-18 13:50:12 +0300

    added ClassWork on lection№4 and practic from seminar№4

--- Examples/Example_from_3rd.lesson/Program.cs
+++ Examples/Example_from_3rd.lesson/Program.cs
@@ -43,3 +43,99 @@ string Method4(int count, string c){
 }
 string res = Method4(10, "Method4 ");
 Console.WriteLine(res);
+
+// цикл for
+
+//метод из предыдущего варианта, но с циклом for
+/*
+string Method4(int count, string c){
+    string result = String.Empty; 
+    for(int i = 0; i < count; i++){
+        result = result + c;
+    }
+    return result;
+}
+string res = Method4(10, "Method4 ");
+Console.WriteLine(res);
+*/
+
+//цтклы в цикле, делаем таблицу умножения 
+/*
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
+*/
+
+//=====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёрточками,
+// маленькие буквы “к” заменить большими “К”,
+// а большие “С” маленькими “с”.
+// Ясна ли задача?
+
+/*
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, ежели бы вас послали вместо нашего милого Винценгероде, вы бы взяли приступом согласие прусского короля. Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012345
+// s[3] = r
+
+string Replace(string text, char oldValue, char newValue){
+    string result = String.Empty;
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        if(text[i] == oldValue) result += $"{newValue}";
+        else result += $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
+*/
+
+//алгоритм сортирования большего к меньшему через min max
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+//метод для вывода массива
+void PrintArray(int[] arr){
+    int count = arr.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+//метод для сортировки
+void SelectionSort(int[] arr){
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        int minPosition = i; //переменная для хранения индекса минимального
+        for (int j = i + 1; j < arr.Length; j++)//цикл для находждения минимального 
+        {
+            if(arr[j] < arr[minPosition]){
+                minPosition = j;
+            }
+        }
+        //меняем местами как обычно
+        int temporary = arr[i];
+        arr[i] = arr[minPosition];
+        arr[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
---
+++ Seminars/Lesson№4/Program.cs
@@ -0,0 +1,126 @@
+﻿//методы и функции
+
+/*
+void SayLang(string name){
+    Console.WriteLine($"привет, {name}");
+}
+SayLang("python");
+SayLang("c#");
+string lang = "c++";
+SayLang(lang);
+
+int SayMax(int num1, int num2){
+    if(num1 > num2) return num1;
+    else return num2;
+}
+//один из вариантов вывода
+Console.WriteLine(SayMax(3, 5));
+//второй вариант вывода
+int num = SayMax(4, 7);
+Console.WriteLine(num);
+//и еще один вариант
+int x = 9, y = 6, num1 = SayMax(x, y);
+Console.WriteLine(num1);
+*/
+
+// Практика
+
+// Задача №24
+/*
+Напишите программу, которая принимает на вход число (А) 
+и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+
+/*
+int SumNumber(int num)
+{
+    int result = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+Console.WriteLine("Vvedite: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumNumber(num));
+*/
+
+// Задача 26
+/*
+ Напишите программу, которая принимает на вход число и выдаёт
+ количество цифр в числе.
+ 456 -> 3
+ 78 -> 2
+ 89126 -> 5
+ */
+
+/*
+int SizeArray(int size)
+{
+    int count = 0;
+    while (size > 0)
+    {
+        size /= 10;
+        count++;
+    }
+    return count;
+}
+Console.WriteLine("Vvedite: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SizeArray(num1));
+*/
+
+// Задача №28
+/*
+ Напишите программу, которая принимает на вход число N и выдаёт
+ произведение чисел от 1 до N.
+4 -> 24 
+5 -> 120
+*/
+
+/*
+int MultNumber(int num)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+Console.WriteLine("Vvedite: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(MultNumber(num3));
+*/
+
+// Задача №30
+/* 
+Напишите программу, которая выводит массив из 8 элементов, 
+ заполненный нулями и единицами в случайном порядке.
+ [1,0,1,1,0,1,0,0]
+ */
+
+/*
+void RandomArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 2); //(от 0 до N-1)
+    }
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+RandomArr(array);
+PrintArray(array);
+*/
