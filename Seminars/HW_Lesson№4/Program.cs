734f824b0f6f38df782657c50183a5d27e1d3c2c
VicS <ruwebman@gmail.com>
2023-05-18 19:11:15 +0300

    Added task №27

--- Seminars/HW_Lesson№4/Program.cs
+++ Seminars/HW_Lesson№4/Program.cs
@@ -1,23 +1,45 @@
-﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+﻿using System;
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-int ExponentiationMethod(int a, int b) 
-{
-    int result = 1;
-    for (int i = 1; i <= b; i++)
-    {
-        result *= a;
-    }
-    return result;
-}
+// int ExponentiationMethod(int a, int b) 
+// {
+//     int result = 1;
+//     for (int i = 1; i <= b; i++)
+//     {
+//         result *= a;
+//     }
+//     return result;
+// }
 
-Console.WriteLine("Введите число A: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число B: ");
-int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число A: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число B: ");
+// int b = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"{a} в степени {b} = {ExponentiationMethod(a, b)}");
+// Console.WriteLine($"{a} в степени {b} = {ExponentiationMethod(a, b)}");
 
-//int res = ExponentiationMethod(2,3);
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// int SumMethod(int a)
+// {
+//     int sum = 0;
+//     while (a > 0)
+//     {
+//         sum += a % 10;
+//         a /= 10;
+//     }
+//     return sum;
+
+// }
+
+// Console.WriteLine("Введите число : ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Сумма цифр в числе {a}  = {SumMethod(a)}");
+
