cbe507f89199f678262214f34ddacf813431123e
VicS <ruwebman@gmail.com>
2023-05-29 23:47:39 +0300

    Added homework №5 tasks 36,38

--- Seminars/HW_Lesson№5/Program.cs
+++ Seminars/HW_Lesson№5/Program.cs
@@ -41,3 +41,94 @@ EvenNumberMethod(array);
 Console.WriteLine($"Чётных чисел в массиве: {EvenNumberMethod(array)}");
 */
 
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+// void CreateArrayMethod(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(0, 10);
+//     }
+// }
+// void PrintArrayMethod(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }    
+
+// int SumEvenPosition(int[] array)
+// {
+//     int sum = 0;
+//      for (int i = 1; i <= array.Length; i = i +2)
+//      {
+//         sum = sum + array[i];
+//      }
+//      return sum;
+// }
+
+// int[] array = new int[8];
+// CreateArrayMethod(array);
+// PrintArrayMethod(array);
+// Console.WriteLine(SumEvenPosition(array));
+
+// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+void CreateArrayMethod(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+}
+void PrintArrayMethod(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}    
+
+int SearchMaxMethod(int[] array)
+{
+    int max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(max < array[i])
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+int SearchMinMethod(int[] array)
+{
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(min > array[i])
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+int[] array = new int[8];
+CreateArrayMethod(array);
+PrintArrayMethod(array);
+int max = SearchMaxMethod(array);
+int min = SearchMinMethod(array);
+int result = max - min;
+Console.WriteLine($"Максимальное значение массива: {max}");
+Console.WriteLine($"Минимальное значение массива: {min}");
+Console.WriteLine($"Разница между максимальным и минимальным элементами: {result}");
