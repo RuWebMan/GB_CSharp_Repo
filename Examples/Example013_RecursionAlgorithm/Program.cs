2d0f7734863000ce9f0a08512c218bac8e7d48d6
VicS <ruwebman@gmail.com>
2023-05-30 21:33:43 +0300

    added some settings

--- Examples/Example013_RecursionAlgorithm/Program.cs
+++ Examples/Example013_RecursionAlgorithm/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 // Двумерные массивы.
 
 //string[,] table = new string[2, 3]; // первая — строки, вторая — столбцы. Двумерный массив чисел.
@@ -7,6 +8,7 @@
 // Работа с таблицей строк.
 
 string[,] newTable = new string[2, 5];
+
 // String.Empty
 // table [0,0] table [0,1] table [0,2] table [0,3] table [0,4]
 // table [1,0] table [1,1] table [1,2] table [0,3] table [1,4]
@@ -55,9 +57,9 @@ newMatrix[2, 3] = 1;
 //            Console.Write($"{matr[i, j]} ");
 //        }
 //       Console.WriteLine();
-//     }      
+//     }
 // }
- 
+
 //  void FillArray(int[,] matr)
 // {
 //     for (int i = 0; i < matr.GetLength(0); i++)
@@ -170,8 +172,10 @@ double Factorial3(int n)
 {
     // 1! = 1
     // 0! = 1
-    if(n == 1) return 1;
-    else return n * Factorial3(n-1);
+    if (n == 1)
+        return 1;
+    else
+        return n * Factorial3(n - 1);
 }
 for (int i = 1; i < 40; i++)
 {
@@ -180,8 +184,10 @@ for (int i = 1; i < 40; i++)
 
 double Fibonacci(int n) // Числа Фибоначи.
 {
-    if(n == 1 || n == 2) return 1;
-    else return Fibonacci(n-1) + Fibonacci(n-2);
+    if (n == 1 || n == 2)
+        return 1;
+    else
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
 for (int i = 1; i < 50; i++)
 {
---
+++ Seminars/Lesson№7/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+void CreateArrayMethod(int[] array)
+{
+    for (int i = 0; i < newMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < newMatrix.GetLength(1); j++)
+        {
+            Console.Write($"{newMatrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintArrayMethod(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите число M: ");
+int row = InputNumber();
+Console.Write("Введите число N: ");
+int colums = InputNumber();
+
+int[,] array = new int[row, colums];
+CraeteArray(array);
+PrintArray(array);
