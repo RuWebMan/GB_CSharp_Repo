37bf01770c404ba3e2a800ef83fe522186600bb5
VicS <ruwebman@gmail.com>
2023-05-31 12:35:03 +0300

    Added task №48,49,51

--- Seminars/Lesson№7/Program.cs
+++ Seminars/Lesson№7/Program.cs
@@ -1,37 +1,181 @@
-﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+﻿using System;
+// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
 // m = 3, n = 4.
 // 1 4 8 19
 // 5 -2 33 -2
 // 77 3 8 1
 
-void CreateArrayMethod(int[,] arr)
-{
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            arr[i, j] = new Random().Next(0, 100);
-        }
-    }
-}
+// void CreateArrayMethod(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i, j] = new Random().Next(0, 100);
+//         }
+//     }
+// }
 
-void PrintArrayMethod(int[,] arr)
-{
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            Console.Write($"{arr[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-}
+// void PrintArrayMethod(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.Write($"{arr[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
-Console.Write("Введите число M: ");
-int row = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число N: ");
-int colums = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число M: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число N: ");
+// int colums = Convert.ToInt32(Console.ReadLine());
 
-int[,] array = new int[row, colums];
-CreateArrayMethod(array);
-PrintArrayMethod(array);
+// int[,] array = new int[row, colums];
+// CreateArrayMethod(array);
+// PrintArrayMethod(array);
+
+//Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве 
+//находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+//m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+// void CreateArrayMethod(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i, j] = i + j;
+//         }
+//     }
+// }
+// void PrintArrayMethod(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.Write($"{arr[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.Write("Введите число M: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число N: ");
+// int colums = Convert.ToInt32(Console.ReadLine());
+
+// int[,] array = new int[row, colums];
+// CreateArrayMethod(array);
+// PrintArrayMethod(array);
+
+// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// void CreateArrayMethod(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i, j] = new Random().Next(0, 10);
+//         }
+//     }
+// }
+// void PrintArrayMethod(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.Write($"{arr[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void ConvertMethod(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             if (i % 2 == 0 && j % 2 == 0)
+//             {
+//                 arr[i, j] = Convert.ToInt32(Math.Pow(arr[i, j], 2));
+//             }
+//         }
+//     }
+// }
+
+// Console.WriteLine();
+// int [,] array = new int [3, 4];
+// CreateArrayMethod(array);
+// PrintArrayMethod(array);
+// Console.WriteLine();
+// ConvertMethod(array);
+// PrintArrayMethod(array);
+// Console.WriteLine();
+
+// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+// void CreateArrayMethod(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i, j] = new Random().Next(0, 10);
+//         }
+//     }
+// }
+// void PrintArrayMethod(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.Write($"{arr[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int SumOfDiagonal(int[,] arr)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             if(i == j)
+//             {
+//                 sum += arr[i, j];
+//             }
+//         }
+//     }
+//     return sum;
+
+// }
+
+// Console.WriteLine();
+// int [,] array = new int[3, 4];
+// CreateArrayMethod(array);
+// PrintArrayMethod(array);
+// Console.WriteLine();
+// Console.WriteLine(SumOfDiagonal(array));
+
+
