32feef24f38dc6319a2aa03d02095e1e888aa360
VicS <ruwebman@gmail.com>
2023-06-06 11:26:45 +0300

    Added homework to seminar №8

---
+++ Seminars/HW_Lesson№8/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void CreateArrayMethod(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArrayMethod(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+ double [,] array = new double[3, 4];
+CreateArrayMethod(array);
+PrintArrayMethod(array);
