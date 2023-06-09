b16debe563b72438e1eb55c9a2d6183bf8222342
VicS <ruwebman@gmail.com>
2023-06-09 23:59:27 +0300

    Added homework & classwork for seminar 9

---
+++ Seminars/HW_Lesson№9/Program.cs
@@ -0,0 +1,111 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+/*
+void PrintNaturalNumbers(int num)
+{
+    if (num == 0)
+        Console.WriteLine($"{num}.");
+    else
+    {
+        Console.Write($"{num}, ");
+        PrintNaturalNumbers(num - 1);
+    }
+}
+Console.WriteLine(); // Добавляю Console.WriteLine для читабельности вывода в терминале.
+Console.Write("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+PrintNaturalNumbers(num);
+Console.WriteLine();
+*/
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Первое решение с рекурсией.
+/*
+Console.WriteLine();
+Console.Write("Введите число М: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int sum = SumNaturalNumbersInInterval(M, N);
+
+int SumNaturalNumbersInInterval(int M, int N)
+{
+    if (M == N)
+    {
+        return M;
+    }
+    else
+    {
+        return M + SumNaturalNumbersInInterval(M + 1, N);
+    }
+}
+Console.WriteLine(
+    $"Cумма значений натуральных элементов в промежутке от {M} до {N} (включительно) равна -  {sum}"
+);
+Console.WriteLine();
+*/
+
+// Второе решение с циклом и проверкой "на дурака".
+
+/*
+Console.WriteLine();
+Console.Write("Введите число М: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+Console.WriteLine();
+
+if (M > N)
+    (M, N) = (N, M);
+for (int i = M; i <= N; i++)
+{
+    sum += i; // Либо прописываем 1 вместо i и получаем колличество натуральных элементов в промежутке от M до N.
+}
+
+Console.WriteLine(
+    $"Cумма значений натуральных элементов в промежутке от {M} до {N} (включительно) равна -  {sum}"
+);
+Console.WriteLine();
+*/
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine();
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int Ackermann(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return Ackermann(m - 1, 1);
+    }
+    else
+    {
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+}
+Console.WriteLine(Ackermann(m, n));
---
+++ Seminars/Lessone№9/Program.cs
@@ -0,0 +1,100 @@
+﻿// Задача 63: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+int InputNumber()
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+void NatureNum(int num)
+{
+    if (num != 0)
+    {
+        NatureNum(num - 1);
+        Console.Write($"{num} ");
+    }
+}
+Console.Write("Введите число N: ");
+int num = InputNumber();
+NatureNum(num);
+
+// Задача 65: Задайте значения M и N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+int InputNumber()
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+void NatureNum(int start, int end)
+{
+    if (end == start)
+        Console.Write($"{start} ");
+    else
+    {
+        NatureNum(start, end - 1);
+        Console.Write($"{end} ");
+    }
+}
+Console.Write("Введите число M: ");
+int num1 = InputNumber();
+Console.Write("Введите число N: ");
+int num2 = InputNumber();
+if (num1 > num2)
+    (num1, num2) = (num2, num1);
+NatureNum(num1, num2);
+
+// Задача 67: Напишите программу, которая будет принимать на вход
+// число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int InputNumber()
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+int ReturnSumNum(int num)
+{
+    if (num == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return ReturnSumNum(num / 10) + num % 10;
+    }
+}
+Console.Write("Введите число M: ");
+int num1 = InputNumber();
+Console.WriteLine(ReturnSumNum(num1));
+
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B,
+// и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+int InputNumber()
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+int ReturnDegreeNum(int num, int num1)
+{
+    if (num1 == 1)
+    {
+        return num;
+    }
+    else
+    {
+        return num * ReturnDegreeNum(num, num1 - 1);
+    }
+}
+Console.Write("Введите число M: ");
+int num1 = InputNumber();
+Console.Write("Введите число N: ");
+int num2 = InputNumber();
+Console.WriteLine(ReturnDegreeNum(num1, num2));
